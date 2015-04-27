cb13ce2e7b7c36f7e6a98bfa66cddd37ed682a05
Camilla-NJU <guhan.1996@qq.com>
2015-04-27 14:31:08 +0800

    frameSize

--- ghost's_count_down/src/ui/Frame.java
+++ ghost's_count_down/src/ui/Frame.java
@@ -4,12 +4,17 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame implements KeyListener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setLayout(null);
+		frame.setSize(1100, 700);
+		frame.setVisible(true);
 	}
 
 	@Override
