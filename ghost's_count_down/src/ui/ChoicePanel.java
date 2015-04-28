276b9ff960ec8bbf5b041bdfb30f711f158ddc17
yuinii1215 <hq14@software.nju.edu.cn>
2015-04-28 19:42:23 +0800

    Some mistakes

--- ghost's_count_down/src/ui/ChoicePanel.java
+++ ghost's_count_down/src/ui/ChoicePanel.java
@@ -3,8 +3,8 @@ package ui;
 import javax.swing.JPanel;
 
 public class ChoicePanel extends JPanel {
-     public SingleButton single = new SingleButton();
+	public SingleButton single = new SingleButton();
      public DoubleButton doub = new DoubleButton();
-    // public ExitButton   exit = new ExitButton();
+     public ExitButton   exit = new ExitButton(40,40);
      public SetButton    set;
 }
