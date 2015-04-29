9efe6091796c84ff33a1f7622208c17331ad4e10
Camilla-NJU <guhan.1996@qq.com>
2015-04-29 21:00:47 +0800

    finalChange

--- ghost's_count_down/src/ui/EnterButton.java
+++ ghost's_count_down/src/ui/EnterButton.java
@@ -2,14 +2,12 @@ package ui;
 
 import java.awt.Graphics;
 import java.awt.Image;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JLabel;
 
-public class EnterButton extends JButton implements MouseListener{
+
+public class EnterButton extends JButton{
 	Image ButtonImage = null;
 	String filePath = null;
 
@@ -22,14 +20,7 @@ public class EnterButton extends JButton implements MouseListener{
 	
 
 	
-	public EnterButton(){
-
-		
-		
-		
-		addMouseListener(this);	
-	}
-		
+			
 	public void ButtonStart(){
 		filePath = "button/enter0.png";
 		ButtonImage = new ImageIcon(filePath).getImage();
@@ -55,37 +46,6 @@ public class EnterButton extends JButton implements MouseListener{
 		g.drawImage(ButtonImage,0,0,this.getWidth(),this.getHeight(),this);
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		isPressed = true;
-		this.repaint();
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		isIn = true;
-		this.repaint();
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		isIn = false;
-		this.repaint();
-	}
+	//@Override
 }
+
